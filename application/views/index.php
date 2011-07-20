ed4a78564d30cf6838f0bf16a8419cfe8f3b746b
Jeduan Cornejo <jeduan@gmail.com>
2011-07-20 05:12:16 -0500

    Cambia por 960gs

--- application/views/index.php
+++ application/views/index.php
@@ -3,273 +3,92 @@
 
 <head>
 	<meta charset="utf-8" />
-	<title>The 1140px Grid &middot; Fluid down to mobile</title>
+	<title>Mirrey or not</title>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	
 	<!-- 1140px Grid styles for IE -->
 	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
 
-	<!-- The 1140px Grid - http://cssgrid.net/ -->
-	<link rel="stylesheet" href="css/1140.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/960gs.css" type="text/css" media="screen" />
 	
 	<!-- Your styles -->
 	<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
-	
-	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
-	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
-	
-	<!--Delete embedded styles, just for example.-->
-	<style type="text/css">
-	
-	body {
-	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-	}
-	
-	.container p {
-	color: #fff;
-	line-height: 100px;
-	background: #000;
-	text-align: center;
-	margin: 20px 0 0 0;
-	}
-	
-	</style>
 
 </head>
 
 
 <body>
+  <div class="container container_10">
+  			<h2 class="grid_10">
+  				10 Column Grid
+  			</h2>
+  			<div class="grid_10">
+  				<p>
+  					735px
+  				</p>
+  			</div>
 
-<div class="container">
-	<div class="row">
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="onecol last">
-			<p>One</p>
-		</div>
-	</div>
-</div>
+  			<div class='clear'>&nbsp;</div>
+  <div class='grid_1'>
+  <p>60px</p>
+  </div><div class='grid_9'>
+  <p>660px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_2'>
+  <p>135px</p>
+  </div><div class='grid_8'>
+  <p>585px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_3'>
+  <p>210px</p>
+  </div><div class='grid_7'>
+  <p>510px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_4'>
+  <p>285px</p>
+  </div><div class='grid_6'>
+  <p>435px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_5'>
+  <p>360px</p>
+  </div><div class='grid_5'>
+  <p>360px</p>
+  </div>
 
-<div class="container">
-	<div class="row">
-		<div class="twocol">
-			<p>Two columns</p>
-		</div>
-		<div class="twocol">
-			<p>Two columns</p>
-		</div>
-		<div class="twocol">
-			<p>Two columns</p>
-		</div>
-		<div class="twocol">
-			<p>Two columns</p>
-		</div>
-		<div class="twocol">
-			<p>Two columns</p>
-		</div>
-		<div class="twocol last">
-			<p>Two columns</p>
-		</div>
-	</div>
-</div>
 
-<div class="container">
-	<div class="row">
-		<div class="threecol">
-			<p>Three columns</p>
-		</div>
-		<div class="threecol">
-			<p>Three columns</p>
-		</div>
-		<div class="threecol">
-			<p>Three columns</p>
-		</div>
-		<div class="threecol last">
-			<p>Three columns</p>
-		</div>
-	</div>
-</div>
 
-<div class="container">
-	<div class="row">
-		<div class="fourcol">
-			<p>Four columns</p>
-		</div>
-		<div class="fourcol">
-			<p>Four columns</p>
-		</div>
-		<div class="fourcol last">
-			<p>Four columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="onecol">
-			<p>One</p>
-		</div>
-		<div class="elevencol last">
-			<p>Eleven columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="twocol">
-			<p>Two columns</p>
-		</div>
-		<div class="tencol last">
-			<p>Ten columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="threecol">
-			<p>Three columns</p>
-		</div>
-		<div class="ninecol last">
-			<p>Nine columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="fourcol">
-			<p>Four columns</p>
-		</div>
-		<div class="eightcol last">
-			<p>Eight columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="fivecol">
-			<p>Five columns</p>
-		</div>
-		<div class="sevencol last">
-			<p>Seven columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="sixcol">
-			<p>Six columns</p>
-		</div>
-		<div class="sixcol last">
-			<p>Six columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="sevencol">
-			<p>Seven columns</p>
-		</div>
-		<div class="fivecol last">
-			<p>Five columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="eightcol">
-			<p>Eight columns</p>
-		</div>
-		<div class="fourcol last">
-			<p>Four columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="ninecol">
-			<p>Nine columns</p>
-		</div>
-		<div class="threecol last">
-			<p>Three columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="tencol">
-			<p>Ten columns</p>
-		</div>
-		<div class="twocol last">
-			<p>Two columns</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="elevencol">
-			<p>Eleven columns</p>
-		</div>
-		<div class="onecol last">
-			<p>One</p>
-		</div>
-	</div>
-</div>
-
-<div class="container">
-	<div class="row">
-		<div class="threecol">
-			<p>Three columns</p>
-		</div>
-		<div class="sixcol">
-			<p>Six columns</p>
-		</div>
-		<div class="threecol last">
-			<p>Three columns</p>
-		</div>
-	</div>
-</div>
+  			<div class='clear'>&nbsp;</div>
+  <div class='grid_1  suffix_9'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_1 suffix_8'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_2 suffix_7'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_3 suffix_6'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_4 suffix_5'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_5 suffix_4'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_6 suffix_3'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_7 suffix_2'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_8 suffix_1'>
+  <p>60px</p>
+  </div><div class='clear'>&nbsp;</div>
+  <div class='grid_1 prefix_9 suffix_0'>
+  <p>60px</p>
+  </div>
+  			<div class="clear">&nbsp;</div>
+  		</div>
 <?php var_dump($me) ?>
 
 </body>
