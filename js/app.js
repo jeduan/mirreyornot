0a41de02fb7d60c1a85510ef0921e5e381cb2b8d
Jeduan Cornejo <jeduan@gmail.com>
2011-07-20 06:23:48 -0500

    Pta que osooo

--- js/app.js
+++ js/app.js
@@ -8,5 +8,48 @@
         console.log('exito');
       });
     });
+    
+    FB.init({appId: $('fb-root').data('appid'), cookie: true});
+
+    FB.getLoginStatus(function(response) {
+        if (response.session) {
+          init();
+        } else {
+          // no user session available, someone you dont know
+        }
+    });
+    function init() {
+        FB.api('/me', function(response) {
+            $("#username").html("<img src='https://graph.facebook.com/" + response.id + "/picture'/><div>" + response.name + "</div>");
+
+            $("#jfmfs-container").jfmfs({ 
+              max_selected: 15, 
+              max_selected_message: "{0} of {1} selected",
+              friend_fields: "id,name,last_name",
+              pre_selected_friends: [1014025367],
+              exclude_friends: [1211122344, 610526078],
+              sorter: function(a, b) {
+                var x = a.last_name.toLowerCase();
+                var y = b.last_name.toLowerCase();
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+              }
+            });
+            $("#jfmfs-container").bind("jfmfs.friendload.finished", function() { 
+                window.console && console.log("finished loading!"); 
+            });
+            $("#jfmfs-container").bind("jfmfs.selection.changed", function(e, data) { 
+                window.console && console.log("changed", data);
+            });                     
+
+            $("#logged-out-status").hide();
+            $("#show-friends").show();
+
+        });
+      }
+
+      $("#show-friends").live("click", function() {
+          var friendSelector = $("#jfmfs-container").data('jfmfs');             
+          $("#selected-friends").html(friendSelector.getSelectedIds().join(', ')); 
+      });
   });
 }(jQuery));
