3babb1abc74abad9c2b64b3bede19bc2ffdf7f65
Jeduan Cornejo <jeduan@gmail.com>
2011-07-20 05:16:49 -0500

    Agrega a mondras papawh

--- application/views/index.php
+++ application/views/index.php
@@ -18,77 +18,13 @@
 
 <body>
   <div class="container container_10">
-  			<h2 class="grid_10">
-  				10 Column Grid
-  			</h2>
-  			<div class="grid_10">
-  				<p>
-  					735px
-  				</p>
-  			</div>
-
-  			<div class='clear'>&nbsp;</div>
-  <div class='grid_1'>
-  <p>60px</p>
-  </div><div class='grid_9'>
-  <p>660px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_2'>
-  <p>135px</p>
-  </div><div class='grid_8'>
-  <p>585px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_3'>
-  <p>210px</p>
-  </div><div class='grid_7'>
-  <p>510px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_4'>
-  <p>285px</p>
-  </div><div class='grid_6'>
-  <p>435px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_5'>
-  <p>360px</p>
-  </div><div class='grid_5'>
-  <p>360px</p>
+    <div class="grid_5">
+      <img src="http://graph.facebook.com/702152773/picture?type=large" alt="" />
+    </div>
+    <div class="grid_5">
+      <img src="http://graph.facebook.com/1002101672/picture?type=large" alt="" />
+    </div>
   </div>
-
-
-
-  			<div class='clear'>&nbsp;</div>
-  <div class='grid_1  suffix_9'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_1 suffix_8'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_2 suffix_7'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_3 suffix_6'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_4 suffix_5'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_5 suffix_4'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_6 suffix_3'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_7 suffix_2'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_8 suffix_1'>
-  <p>60px</p>
-  </div><div class='clear'>&nbsp;</div>
-  <div class='grid_1 prefix_9 suffix_0'>
-  <p>60px</p>
-  </div>
-  			<div class="clear">&nbsp;</div>
-  		</div>
 <?php var_dump($me) ?>
 
 </body>
