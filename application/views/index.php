66a25629ca57cd312b40170cc4a00ee927eb0003
Jeduan Cornejo <jeduan@gmail.com>
2011-07-20 05:19:04 -0500

    cleanup y muchos estilos

--- application/views/index.php
+++ application/views/index.php
@@ -5,7 +5,6 @@
 	<meta charset="utf-8" />
 	<title>Mirrey or not</title>
 
-	<!-- 1140px Grid styles for IE -->
 	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
 
 	<link rel="stylesheet" href="css/960gs.css" type="text/css" media="screen" />
@@ -18,15 +17,14 @@
 
 <body>
   <div class="container container_10">
+    <h1 class="grid_10">¡Vota por tu Mirrey, papawh!</h1>
     <div class="grid_5">
-      <img src="http://graph.facebook.com/702152773/picture?type=large" alt="" />
+      <img src="http://graph.facebook.com/702152773/picture?type=large" alt="" class="profile"/>
     </div>
     <div class="grid_5">
-      <img src="http://graph.facebook.com/1002101672/picture?type=large" alt="" />
+      <img src="http://graph.facebook.com/1002101672/picture?type=large" alt="" class="profile"/>
     </div>
   </div>
-<?php var_dump($me) ?>
-
 </body>
 
 </html>
