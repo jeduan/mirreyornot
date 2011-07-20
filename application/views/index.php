6524f0339833255af83b15744ac799c61a5001fb
Jeduan Cornejo <jeduan@gmail.com>
2011-07-20 05:33:18 -0500

    agrega forma con values hidden

--- application/views/index.php
+++ application/views/index.php
@@ -20,11 +20,19 @@
     <h1 class="grid_10">¡Vota por tu Mirrey, papawh!</h1>
     <div class="grid_5">
       <img src="http://graph.facebook.com/702152773/picture?type=large" alt="" class="profile"/>
-      <button class="votar" >Pta, este, papawh</button>
+      <form>
+      <input type="hidden" name="votado" value="702152773" />
+      <input type="hidden" name="votante" value="<?php echo $me['uid'] ?>" />
+      <input type="send" value="Pta, este, papawh" >
+      </form>
     </div>
     <div class="grid_5">
       <img src="http://graph.facebook.com/1002101672/picture?type=large" alt="" class="profile"/>
-      <button class="votar" >Wey este es de mejor familia</button>
+      <form>
+      <input type="hidden" name="votado" value="1002101672" />
+      <input type="hidden" name="votante" value="<?php echo $me['uid'] ?>" />
+      <input type="send" value="Wey este es de mejor familia" >
+      </form>
     </div>
   </div>
 </body>
