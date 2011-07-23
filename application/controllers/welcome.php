8cd17bb9eb73f8bc311b0e6a6f00b220a4e5bf88
Jeduan Cornejo <jeduan@gmail.com>
2011-07-23 12:43:01 -0500

    Convierte tabs en espacios

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -7,79 +7,79 @@ class Welcome extends CI_Controller {
     $this->load->model('mirrey');
   }
 
-	public function index() {
-		$this->config->load('facebook');
-		$fb_config = array(
-			'appId' => $this->config->item('app_id'),
-			'secret' => $this->config->item('app_secret')
-		);
-		$this->load->library('Facebook', $fb_config);
-		
-		$user = $this->facebook->getUser();
-		
-		if (! $user) {
-  		$data['loginUrl'] = $this->facebook->getLoginUrl(array('scope' => 'publish_stream'));
-  		$this->load->view('loggedOut', $data);
-  		return;
-  	}
-  	
-	  $profile = null;
-	  
-		try {
-			$profile = $this->facebook->api('/me?fields=id,name,link');					
-		} catch (FacebookApiException $e) {
-			error_log($e);
-			$user = null;
-		}
-		
-		$fb_data = array(
-			'me' => $profile,
-			'uid' => $user
-	  );
-		$this->session->set_userdata('fb_data', $fb_data);
+  public function index() {
+    $this->config->load('facebook');
+    $fb_config = array(
+      'appId' => $this->config->item('app_id'),
+      'secret' => $this->config->item('app_secret')
+    );
+    $this->load->library('Facebook', $fb_config);
+    
+    $user = $this->facebook->getUser();
+    
+    if (! $user) {
+      $data['loginUrl'] = $this->facebook->getLoginUrl(array('scope' => 'publish_stream'));
+      $this->load->view('loggedOut', $data);
+      return;
+    }
+    
+    $profile = null;
+    
+    try {
+      $profile = $this->facebook->api('/me?fields=id,name,link');					
+    } catch (FacebookApiException $e) {
+      error_log($e);
+      $user = null;
+    }
+    
+    $fb_data = array(
+      'me' => $profile,
+      'uid' => $user
+    );
+    $this->session->set_userdata('fb_data', $fb_data);
     
     $participants = $this->mirrey->los_participantes();
-		
-		$data = array_merge($fb_data, array(
-		  'app_id' => $this->config->item('app_id'),
-		  'participants' => $participants,
-		  'message' => $this->session->userdata('message')
-		));
-		$this->load->view('index', $data);			
-	
-	}
-	
-	public function topten() {
-	  $top = $this->mirrey->los_mejores_10();
-	  echo json_encode($top);
-	}
-	
-	public function participants() {
-	  $participants = $this->mirrey->los_participantes();
-	  echo json_encode($participants);
-	}
-	
-	public function add() {
-	  $name = $this->input->post("name");
-	  $id = $this->input->post("id");
-	  
-	  $insert = $this->mirrey->insertame_papawh($name, $id);
-	  echo json_encode($insert);
-	}
-	
-	public function vote() {
-	  $votado =  $this->input->post("votado");
-	  $votante = $this->input->post("votante");
-		$vote = $this->mirrey->valida_papawh($votado, $votante);
-		
-	  $message = ($vote) ?		  
-		  'Pta si' :
-		  'Ya votaste papawh';
-		  		
-		echo json_encode(array(
-		    'vote' => $vote, 
-		    'message' => $message));
-	}
+    
+    $data = array_merge($fb_data, array(
+      'app_id' => $this->config->item('app_id'),
+      'participants' => $participants,
+      'message' => $this->session->userdata('message')
+    ));
+    $this->load->view('index', $data);			
+  
+  }
+  
+  public function topten() {
+    $top = $this->mirrey->los_mejores_10();
+    echo json_encode($top);
+  }
+  
+  public function participants() {
+    $participants = $this->mirrey->los_participantes();
+    echo json_encode($participants);
+  }
+  
+  public function add() {
+    $name = $this->input->post("name");
+    $id = $this->input->post("id");
+    
+    $insert = $this->mirrey->insertame_papawh($name, $id);
+    echo json_encode($insert);
+  }
+  
+  public function vote() {
+    $votado =  $this->input->post("votado");
+    $votante = $this->input->post("votante");
+    $vote = $this->mirrey->valida_papawh($votado, $votante);
+    
+    $message = ($vote) ?		  
+      'Pta si' :
+      'Ya votaste papawh';
+          
+    echo json_encode(array(
+        'vote' => $vote, 
+        'message' => $message));
+  }
 }
 
 /* End of file welcome.php */
--- application/models/mirrey.php
+++ application/models/mirrey.php
@@ -1,61 +1,61 @@
 <?php 
 class Mirrey extends CI_Model{
-	
-	function __construct() {
-	  parent::__construct();
-	}
-	
-	function los_participantes($must = null) {
-	  $candidates = array('702152773', '2250617', '572862449', '502180815', '1002101672', '734322104');
-	  $keys = array_rand($candidates, 2);
-	  $ret = array();
-	  foreach ($keys as $key)
-	  {
-	    $ret []= $candidates[$key];
-	  }
-	  
-	  return $ret;
-	}
-	
-	//TODO escribir esto en ActiveRecord ?
-	function los_mejores_10() {
-	  $sql = "SELECT `votado` as id, COUNT(`votado`) as total"
-	      . " FROM `votos`"
-	      . " GROUP BY `votado`"
-	      . " ORDER BY total DESC LIMIT 10";
-	  $query = $this->db->query($sql);
-	  
-	  return array('data' => $query->result_array());
-	}
-	
-	function insertame_papawh($name, $id){
+  
+  function __construct() {
+    parent::__construct();
+  }
+  
+  function los_participantes($must = null) {
+    $candidates = array('702152773', '2250617', '572862449', '502180815', '1002101672', '734322104');
+    $keys = array_rand($candidates, 2);
+    $ret = array();
+    foreach ($keys as $key)
+    {
+      $ret []= $candidates[$key];
+    }
+    
+    return $ret;
+  }
+  
+  //TODO escribir esto en ActiveRecord ?
+  function los_mejores_10() {
+    $sql = "SELECT `votado` as id, COUNT(`votado`) as total"
+        . " FROM `votos`"
+        . " GROUP BY `votado`"
+        . " ORDER BY total DESC LIMIT 10";
+    $query = $this->db->query($sql);
+    
+    return array('data' => $query->result_array());
+  }
+  
+  function insertame_papawh($name, $id){
     $this->db->where('name', $name);
     $this->db->where('id', $id);
     $query = $this->db->get("mirrey");
     
     if ($query->num_rows() > 0) {
-    	return false;
+      return false;
     } else {
-  		$a = array(
-  			"name" => $name,
-  			"id" => $id);  			
-  		return ($this->db->insert("mirrey", $a));
-  	}
-	}
-		
-	function valida_papawh($votado, $votante){
-		$this->db->where('votado', $votado);
-		$this->db->where('votante', $votante);
-		$query = $this->db->get("votos");		
-		
-		if ($query->num_rows() > 0) {
-			return false;
-		} else {
-			$a = array( 
+      $a = array(
+        "name" => $name,
+        "id" => $id);  			
+      return ($this->db->insert("mirrey", $a));
+    }
+  }
+    
+  function valida_papawh($votado, $votante){
+    $this->db->where('votado', $votado);
+    $this->db->where('votante', $votante);
+    $query = $this->db->get("votos");		
+    
+    if ($query->num_rows() > 0) {
+      return false;
+    } else {
+      $a = array( 
         "votado" => $votado,
-  			"votante" => $votante);
-			return ($this->db->insert("votos", $a));
-		}						
-	}	
+        "votante" => $votante);
+      return ($this->db->insert("votos", $a));
+    }						
+  }	
 }
 ?>
